089bbd698dcae24b17bb9a4cb2bbe8f5183062e3
Roman <r.monahov2014@yandex.ru>
2021-01-13 10:01:52 +0300

    Initial Commit

---
+++ Horoscope/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Horoscope
+//
+//  Created by Роман on 13.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
